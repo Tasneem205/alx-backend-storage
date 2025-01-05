cbc2ba7c7bf5593aeb18252a735cbab939e8e5d5
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-05 07:15:45 +0200

    0x00 task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- select special style
+SELECT band_name,
+CASE
+    WHEN split IS NULL THEN 2022 - formed
+    ELSE split - formed
+END AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
