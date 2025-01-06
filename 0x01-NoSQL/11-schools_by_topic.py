15d186e4c35e50228ac939abc604d1c97184d36f
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-06 19:38:25 +0200

    0x01 task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" search by topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """find with a where clause"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find({"topic": topic}))
