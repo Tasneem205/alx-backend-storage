c449d39ccee88e94ef705cf82ab4978bd431c596
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-06 02:16:36 +0200

    0x00 task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- save div funciton
+
+DROP FUNCTION IF EXISTS SaveDiv;
+
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURN FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
