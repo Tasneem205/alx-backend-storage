fba25513e27b19bc2d71f58edd2bb9024b387b8f
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-06 19:13:18 +0200

    0x01 task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""insert new collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert new collection function"""
+    if not mongo_collection:
+        return None;
+    return result.inserted_id
