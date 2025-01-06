06a45e4b3f96ce138a644ecfbbf39e470d663b6b
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-06 23:33:42 +0200

    0x01 task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-"""  """
+""" list all file """
 
 
 def list_all(mongo_collection):
     """list all"""
-    if not mongo_collection:
-        return []
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in methods:
+        method_counts[method] = collection.count_documents({"method": method})
+
+    s = collection.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
