0dfb13c21da7d0ddc320550b9f9e378b692cc982
Tasneem205 <tasneemabdeltawab205@gmail.com>
2025-01-07 22:00:10 +0200

    0x02 task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ from typing import Union, Callable, Optional
 def call_history(method: Callable) -> Callable:
     """ call history """
     @wraps(method)
-    def wrapper(self, *arg, **kwargs):
+    def wrapper(self, *args, **kwargs):
         input_key = f"{method.__qualname__}:inputs"
         output_key = f"{method.__qualname__}:outputs"
         self._redis.rpush(input_key, str(args))
